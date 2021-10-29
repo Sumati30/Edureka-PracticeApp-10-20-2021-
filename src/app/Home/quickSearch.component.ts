f1ae6d5dc53565bd55c494239f770882ff1f0b10
Sumati30 <sumati.khed30@gmail.com>
2021-10-29 08:31:28 -0700

    NFA

--- src/app/Home/quickSearch.component.ts
+++ src/app/Home/quickSearch.component.ts
@@ -7,5 +7,35 @@ import {Component} from '@angular/core';
 })
 
 export class QuickSearchComponent{
-    
+    hotelType : any[] = 
+    [
+        {
+            "_id":1,
+            "trip":"1",
+            "name": "Business",
+            "image" : "./assets/HotelAPI/buisnessTrips.jpg",
+            "cost" : 5000
+        },
+        {
+            "_id":2,
+            "trip":"2",
+            "name": "Holiday",
+            "image" : "./assets/HotelAPI/holidayTrip.jpg ",
+            "cost" : 3000
+        },
+        {
+            "_id":3,
+            "trip":"3",
+            "name": "Travel",
+            "image" : "./assets/HotelAPI/travelTrip.jpg ",
+            "cost" : 2000
+        },
+        {
+            "_id":4,
+            "trip":"4",
+            "name": "Luxury",
+            "image" : "./assets/HotelAPI/luxury.jpg ",
+            "cost" : 10000
+        }
+    ]
 }
--- src/app/Home/search.component.ts
+++ src/app/Home/search.component.ts
@@ -7,5 +7,43 @@ import {Component} from '@angular/core';
 })
 
 export class SearchComponent{
-    
+    title : string = "Book your trip with Us";
+    locations : any[] = [
+        {
+            "_id" : 1,
+            "city_name" : "Goa",
+            "city" : 1,
+            "country_name" : "India" 
+        },
+        {
+            "_id" : 2,
+            "city_name" : "Indore",
+            "city" : 2,
+            "country_name" : "India" 
+        },
+        {
+            "_id" : 3,
+            "city_name" : "Hyderabad",
+            "city" : 3,
+            "country_name" : "India" 
+        },
+        {
+            "_id" : 4,
+            "city_name" : "Kolkatta",
+            "city" : 4,
+            "country_name" : "India" 
+        },
+        {
+            "_id" : 5,
+            "city_name" : "Mumbai",
+            "city" : 5,
+            "country_name" : "India" 
+        },
+        {
+            "_id" : 6,
+            "city_name" : "Bangalore",
+            "city" : 6,
+            "country_name" : "India" 
+        },
+    ]
 }
